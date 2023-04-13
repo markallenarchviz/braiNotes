5b7c2535d356a9c4129ded3b79f8031b9bd4b49d
Mark Allen <markallenarchviz@gmail.com>
2023-04-13 01:38:10 -0300

    mongoDB environment variable

--- src/config/conn.js
+++ src/config/conn.js
@@ -1,9 +1,12 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
+
+const pass = process.env.MONGODB_PASSWORD;
 
 async function main() {
     try {
         await mongoose.set('strictQuery', true);
-        await mongoose.connect('mongodb+srv://markallenarchviz:ZDV6gl2TsqLL3EiT@cluster0.sovxgpu.mongodb.net/?retryWrites=true&w=majority')
+        await mongoose.connect(`mongodb+srv://markallenarchviz:${pass}@cluster0.sovxgpu.mongodb.net/?retryWrites=true&w=majority`)
         console.log('conectou');
     } catch (error) {
         console.log(error);
