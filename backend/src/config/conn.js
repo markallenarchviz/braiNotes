4b046f27059f57cbf1ca86311fe32fec6eb71cbd
Mark Allen <markallenarchviz@gmail.com>
2023-04-14 01:31:53 -0300

    .env

--- backend/src/config/conn.js
+++ backend/src/config/conn.js
@@ -6,7 +6,7 @@ const pass = process.env.MONGODB_PASSWORD;
 async function main() {
     try {
         await mongoose.set('strictQuery', true);
-        await mongoose.connect(`mongodb+srv://markallenarchviz:IMf0xUvVwpreEvSv@cluster0.unqxatn.mongodb.net/?retryWrites=true&w=majority`)
+        await mongoose.connect(`mongodb+srv://markallenarchviz:${pass}@cluster0.unqxatn.mongodb.net/?retryWrites=true&w=majority`)
         console.log('conectou');
     } catch (error) {
         console.log(error);
